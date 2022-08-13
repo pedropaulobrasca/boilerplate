cc626707b72295ee9110f1c4d8bad8db0fb4d0f0
Pedro Paulo <pedropaulobrasca@gmail.com>
2022-08-13 17:24:24 -0300

    Husky config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,6 @@ type Props = {
   title?: string
 }
 
-const foo = 'bla'
-
 export default function Home({ title = 'React Avancado!' }: Props) {
   return <div className={styles.container}>{title}</div>
 }
