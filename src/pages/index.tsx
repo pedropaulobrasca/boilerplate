966c10d3fe475a38f240607e6215a46c3ab7b87b
Pedro Paulo <pedropaulobrasca@gmail.com>
2022-08-13 15:12:41 -0300

    🚀 Init commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import styles from '../../styles/Home.module.css'
+
+type Props = {
+  title?: string
+}
+
+const foo = "bla";
+
+export default function Home({title = 'React Avancado!'}: Props) {
+  return (
+    <div className={styles.container}>
+      {title}
+    </div>
+  )
+}
