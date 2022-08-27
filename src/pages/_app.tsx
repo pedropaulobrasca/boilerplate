00f02795ec1bb9aeb318874a53e8794d333f538d
Pedro Paulo <pedropaulobrasca@gmail.com>
2022-08-27 18:13:22 -0300

    PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Boilerplate Next.js</title>
         <link rel="icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project starter to work witch TypeScript, React, NextJS and Styled-Components"
