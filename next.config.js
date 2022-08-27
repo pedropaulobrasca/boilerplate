cf7648aa84bf83d8720dfc266ce2e26d54105fbc
Pedro Paulo <pedropaulobrasca@gmail.com>
2022-08-27 18:21:02 -0300

    Updates e desativando PWA

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true
+}
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: !isProd
-  }
-})
+module.exports = nextConfig
